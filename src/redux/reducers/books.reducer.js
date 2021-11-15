c1f44a144c42fb8e0c9bc55e02aef92f47fc6efa
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-15 14:39:23 -0600

    booksReducer made and added to rootReducer

---
+++ src/redux/reducers/books.reducer.js
@@ -0,0 +1,12 @@
+const booksReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_BOOKS':
+            return action.payload;
+        case 'LOGOUT':
+            return [];
+        default:
+            return state;
+    }
+};
+
+export default books;
