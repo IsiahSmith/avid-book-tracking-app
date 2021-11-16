39e0a3ed1abb00d543aabb3769d491088a91647c
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-16 10:48:44 -0600

    updateBook made in saga

--- src/redux/sagas/books.saga.js
+++ src/redux/sagas/books.saga.js
@@ -5,27 +5,38 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* fetchBooks() {
     try {
         const response = yield axios.get('/api/books')
-        yield put({ type: 'SET_BOOKS', payload: response.data});
+        yield put({ type: 'SET_BOOKS', payload: response.data });
     } catch (err) {
-        console.log('GET ERROR', err);
+        console.log('GET ERROR IN SAGA', err);
     }
 };
 
-//sage POST to server
+//saga POST to server
 function* addBook(action) {
     try {
         yield axios.post('/api/books', action.payload)
         yield put({ type: 'FETCH_BOOKS' })
     } catch (err) {
-        console.log('POST ERROR', err);
+        console.log('POST ERROR IN SAGA', err);
     }
 };
+
+//saga PUT to server
+function* updateBook(action) {
+    try {
+        yield axios.put(`/api/books/${action.payload.id}`)
+        yield put({ type: 'FETCH_BOOKS' })
+    } catch (err) {
+        console.log('PUT ERROR IN SAGA', err);
+    }
+}
 
 //watching for functions to be called
 function* booksSaga() {
     yield takeLatest('FETCH_BOOKS', fetchBooks);
     yield takeLatest('ADD_BOOK', addBook);
-}
+    yield takeLatest('UPDATE_BOOK', updateBook);
+} 
 
 
 export default booksSaga;
