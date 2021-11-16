235b1e3bf2656fc2fefe40ea73cadaca7e2382f7
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-16 09:35:32 -0600

    AddBook form fully working

--- src/components/AddBook/AddBook.jsx
+++ src/components/AddBook/AddBook.jsx
@@ -1,8 +1,50 @@
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
+function AddBook() {
+    const dispatch = useDispatch();
 
-function AddBook(params) {
+    const [newBook, setNewBook] = useState({ title: '', author: '', page_count: '' })
+
+    //Sets newBook local state to the passed in inputs
+    const handlePropertyChange = (event, property) => {
+        setNewBook({...newBook, [property]: event.target.value})
+    };
+
+    //Sends new book to the saga
+    const addNewBook = (event) => {
+        event.preventDefault();
+        dispatch({ type: 'ADD_BOOK', payload: newBook});
+    };
+
     return (
-        <p>Add Book</p>
+        <>
+            <form onSubmit={addNewBook}>
+                <input
+                    placeholder="Title"
+                    type="text"
+                    value={newBook.title}
+                    onChange={(event) => handlePropertyChange(event, 'title')}
+                />
+                <input
+                    placeholder="Author"
+                    type="text"
+                    value={newBook.author}
+                    onChange={(event) => handlePropertyChange(event, 'author')}
+                />
+                <input
+                    placeholder="Page Count"
+                    type="text"
+                    value={newBook.page_count}
+                    onChange={(event) => handlePropertyChange(event, 'page_count')}
+                />
+                <button
+                    type="submit">Add New Book!</button>
+
+            </form>
+
+        </>
     )
 }
 
