579970c0e7f973679a512747a56347916c213042
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-17 10:09:59 -0600

    GET and POST for reading_session table working except for book_id

--- src/components/UpdateProgress/UpdateProgress.jsx
+++ src/components/UpdateProgress/UpdateProgress.jsx
@@ -1,8 +1,51 @@
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
+function UpdateProgress() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [readingSession, setReadingSession] = useState({ date: '', duration: '', page: '' })
 
-function UpdateProgress(params) {
+    //Sets newBook local state to the passed in inputs
+    const handlePropertyChange = (event, property) => {
+        setReadingSession({...readingSession, [property]: event.target.value})
+    };
+
+    //Sends new book to the saga
+    const addReadingSession = (event) => {
+        event.preventDefault();
+        dispatch({ type: 'ADD_PROGRESS', payload: readingSession});
+        history.push('/homepage');
+    };
+
     return (
-        <p>Update Progress</p>
+        <>
+        <button onClick={() => history.push('/homepage')}>Cancel</button>
+        <h2>Update Progress</h2>
+            <form onSubmit={addReadingSession}>
+                <input
+                    placeholder="Date"
+                    type="date"
+                    value={readingSession.date}
+                    onChange={(event) => handlePropertyChange(event, 'date')}
+                />
+                <input
+                    placeholder="Duration"
+                    type="number"
+                    value={readingSession.duration}
+                    onChange={(event) => handlePropertyChange(event, 'duration')}
+                />
+                <input
+                    placeholder="Page"
+                    type="number"
+                    value={readingSession.page}
+                    onChange={(event) => handlePropertyChange(event, 'page')}
+                />
+                <button type="submit">UPDATE</button>
+            </form>
+
+        </>
     )
 }
 
