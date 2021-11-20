77b6658cf980afd5bb5d4b8b96fa708af52abd36
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-19 19:49:18 -0600

    progressForBook variable made to filter out progress of completed books

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -14,8 +14,8 @@ function HomePage() {
         dispatch({ type: 'FETCH_PROGRESS' });
     }, []);
 
-    let currentlyReading = books.filter(book => book.rating === null)
-    // let progressForBook = progress.filter(session => session.book_id === )
+    let currentlyReading = books.filter(book => book.rating === null);
+    let progressForBook = progress.filter(session => session.rating === null);
 
     console.log('progress is', progress);
     return (
@@ -29,13 +29,13 @@ function HomePage() {
                             <button onClick={() => history.push(`/update/${book.id}`)}>UPDATE PROGRESS</button>
                             <button onClick={() => history.push(`/complete/${book.id}`)}>COMPLETE</button>
                         </div>
-                        {progress.map((session, i) => (
+                        {progressForBook.map((session, i) => (
                             <div key={i}>
                                 {session.book_id === book.id ?
                                     <div>
                                         Currently on page {session.page} { }
                                         Last read on {session.date}
-                                    </div> : <p>No progress added yet</p>}
+                                    </div> : <div>No progress added yet</div>}
                             </div>
                         ))} 
                     </div>
