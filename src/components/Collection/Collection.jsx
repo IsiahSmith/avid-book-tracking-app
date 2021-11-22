ad125a9857b187cb2da2734869bafbadd8fe8a00
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-22 15:00:20 -0600

    collection list has reading session data

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -12,19 +12,29 @@ function Collection() {
         dispatch({ type: 'FETCH_PROGRESS' });
     }, []);
 
-    let completedBooks = books.filter(book => book.rating > 0)
+    let completedBooks = books.filter(book => book.rating > 0);
+    let progressForBook = progress.filter(session => session.rating > 0);
 
     return (
         <div>
             <h2>Collection</h2>
-            {completedBooks.map((book) => (
-                
-                <div key={book.id}>
-                    <div>{book.title}, by {book.author} || <span>Rating: {book.rating}</span>
-                    <button onClick={() => dispatch({ type: "DELETE_BOOK", payload: book })}>DELETE</button>
+            {completedBooks.map((book) => {
+
+                let completedBookSessions = progressForBook.filter(session => session.book_id === book.id);
+                let finalSession = completedBookSessions[completedBookSessions.length - 1];
+
+                return (
+                    <div key={book.id}>
+                        <div>{book.title}, by {book.author} || <span>Rating: {book.rating}</span>
+                            <button onClick={() => dispatch({ type: "DELETE_BOOK", payload: book })}>DELETE</button>
+                        </div>
+                        <div>
+                            <p>Read Book in -total hours-</p>
+                            <p>Finished on {finalSession.date.split('T')[0]}</p>
+                        </div>
                     </div>
-                </div>
-            ))}
+                )
+            })}
         </div>
     );
 }
