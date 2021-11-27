ed0f375de79c71fd62793902fb56a66de94ccf42
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-27 17:42:53 -0600

    added grid to homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -4,6 +4,8 @@ import { useHistory } from 'react-router-dom';
 
 //Material-UI imports
 import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import Grid from '@mui/material/Grid';
 
 function HomePage() {
     const dispatch = useDispatch();
@@ -22,31 +24,40 @@ function HomePage() {
 
     return (
         <>
-            <h2>Currently Reading</h2>
-            {books.length > 0 ? <div>
-                {currentlyReading.map((book) => {
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                style={{ minHeight: '30vh' }}
+            >
+                <h2>Currently Reading</h2>
+                {books.length > 0 ? <div>
+                    {currentlyReading.map((book) => {
 
-                    let bookSessions = progressForBook.filter(session => session.book_id === book.id);
-                    let recentSession = bookSessions[bookSessions.length-1];
+                        let bookSessions = progressForBook.filter(session => session.book_id === book.id);
+                        let recentSession = bookSessions[bookSessions.length - 1];
 
-                    return (
-                        <div key={book.id}>
-                            <div>{book.title}, by {book.author}
-                                <Button variant='outlined' onClick={() => history.push(`/edit/${book.id}`)}>EDIT</Button>
-                                <Button variant='outlined' onClick={() => history.push(`/update/${book.id}`)}>UPDATE PROGRESS</Button>
-                                <Button variant='outlined' onClick={() => history.push(`/complete/${book.id}`)}>COMPLETE</Button>
+                        return (
+                            <div key={book.id}>
+                                <div>{book.title}, by {book.author}
+                                    <Button variant='outlined' onClick={() => history.push(`/edit/${book.id}`)}>EDIT</Button>
+                                    <Button variant='outlined' onClick={() => history.push(`/update/${book.id}`)}>UPDATE PROGRESS</Button>
+                                    <Button variant='outlined' onClick={() => history.push(`/complete/${book.id}`)}>COMPLETE</Button>
+                                </div>
+                                {recentSession &&
+                                    <div>
+                                        <p>Currently on Page {recentSession?.page}</p>
+                                        <p>Last Read on {recentSession?.date.split('T')[0]}</p>
+                                        <p>Logged {recentSession?.duration} hours of reading last session</p>
+                                    </div>}
                             </div>
-                            {recentSession && 
-                            <div>
-                                <p>Currently on Page {recentSession?.page}</p>
-                                <p>Last Read on {recentSession?.date.split('T')[0]}</p>
-                                <p>Logged {recentSession?.duration} hours of reading last session</p>
-                            </div>}
-                        </div>
-                    )
-                })}
-            </div> : <p> You're not tracking any books yet! Click 'ADD BOOK' to get started!</p>}
-            <Button variant='contained' onClick={() => history.push('/addbook')}>ADD BOOK</Button>
+                        )
+                    })}
+                </div> : <p> You're not tracking any books yet! Click 'ADD BOOK' to get started!</p>}
+                <Button variant='contained' onClick={() => history.push('/addbook')}>ADD BOOK</Button>
+            </Grid>
         </>
     );
 }
