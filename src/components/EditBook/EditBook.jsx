868d295b9173befe41946a015e28310006ccf033
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-27 15:11:15 -0600

    updated layout for EditBook

--- src/components/EditBook/EditBook.jsx
+++ src/components/EditBook/EditBook.jsx
@@ -1,16 +1,18 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
-import { useHistory, useParams  } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 //Material-UI imports
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Grid from '@mui/material/Grid';
 
 function EditBook() {
     const dispatch = useDispatch();
     const history = useHistory();
     const [updateBook, setUpdateBook] = useState({ title: '', author: '', page_count: '' })
-    const {book_id} = useParams();
+    const { book_id } = useParams();
 
     //Sets updateBook local state to the passed in inputs
     const handlePropertyChange = (event, property) => {
@@ -20,41 +22,80 @@ function EditBook() {
     //Sends updated book information to the saga
     const runUpdateBook = (event) => {
         event.preventDefault();
-        dispatch({ type: 'UPDATE_BOOK', payload: {...updateBook, book_id }});
+        dispatch({ type: 'UPDATE_BOOK', payload: { ...updateBook, book_id } });
         history.push('/homepage');
     };
 
     return (
         <>
-            <Button variant='contained' onClick={() => history.push('/homepage')}>Cancel</Button>
-            <h2>Edit Book</h2>
-            <form onSubmit={runUpdateBook}>
-                <TextField
-                    id="outlined-basic" 
-                    variant="outlined"
-                    label="Title"
-                    type="text"
-                    value={updateBook.title}
-                    onChange={(event) => handlePropertyChange(event, 'title')}
-                />
-                <TextField
-                    id="outlined-basic" 
-                    variant="outlined"
-                    label="Author"
-                    type="text"
-                    value={updateBook.author}
-                    onChange={(event) => handlePropertyChange(event, 'author')}
-                />
-                <TextField
-                    id="outlined-basic" 
-                    variant="outlined"
-                    label="Page Count"
-                    type="number"
-                    value={updateBook.page_count}
-                    onChange={(event) => handlePropertyChange(event, 'page_count')}
-                />
-                <Button variant='contained' type="submit">SUBMIT</Button>
-            </form>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                style={{ minHeight: '30vh' }}
+            >
+                <h2>Edit Book</h2>
+                <FormControl
+                    sx={{
+                        width: '40ch',
+
+                    }}
+                    onSubmit={runUpdateBook}
+                >
+                    <TextField
+                        sx={{ m: 1 }}
+                        id="outlined-basic"
+                        variant="outlined"
+                        label="Title"
+                        type="text"
+                        value={updateBook.title}
+                        onChange={(event) => handlePropertyChange(event, 'title')}
+                    />
+                    <TextField
+                        sx={{ m: 1 }}
+                        id="outlined-basic"
+                        variant="outlined"
+                        label="Author"
+                        type="text"
+                        value={updateBook.author}
+                        onChange={(event) => handlePropertyChange(event, 'author')}
+                    />
+                    <TextField
+                        sx={{ m: 1 }}
+                        id="outlined-basic"
+                        variant="outlined"
+                        label="Page Count"
+                        type="number"
+                        helperText="(How many pages are in the book)"
+                        value={updateBook.page_count}
+                        onChange={(event) => handlePropertyChange(event, 'page_count')}
+                    />
+                    <Grid
+                        container
+                        spacing={0}
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Button
+                            variant='contained'
+                            sx={{
+                                m: .5,
+                                width: '21ch',
+                            }}
+                            onClick={() => history.push('/homepage')}>Cancel</Button>
+                        <Button
+                            variant='contained'
+                            sx={{
+                                m: .5,
+                                width: '21ch',
+                            }}
+                            type="submit">SUBMIT</Button>
+                    </Grid>
+                </FormControl>
+            </Grid>
         </>
     )
 }
