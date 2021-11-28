c59c2434cee50a4cad4904a984268032a5ae38fb
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-28 16:48:54 -0600

    added MUI to new table

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -4,8 +4,14 @@ import { useHistory } from 'react-router-dom';
 
 //Material-UI imports
 import Button from '@mui/material/Button';
-import FormControl from '@mui/material/FormControl';
 import Grid from '@mui/material/Grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function HomePage() {
     const dispatch = useDispatch();
@@ -33,40 +39,41 @@ function HomePage() {
                 style={{ minHeight: '30vh' }}
             >
                 <h2>Currently Reading</h2>
-                {books.length > 0 ? <table>
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Page#</th>
-                            <th>Last Read</th>
-                            <th>%Complete</th>
-                            <th>Edit</th>
-                            <th>Update Progress</th>
-                            <th>Complete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                {books.length > 0 ? <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Title</TableCell>
+                            <TableCell>Author</TableCell>
+                            <TableCell>Page#</TableCell>
+                            <TableCell>Last Read</TableCell>
+                            <TableCell>%Complete</TableCell>
+                            <TableCell>Edit</TableCell>
+                            <TableCell>Update Progress</TableCell>
+                            <TableCell>Complete</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
                         {currentlyReading.map((book) => {
 
                             let bookSessions = progressForBook.filter(session => session.book_id === book.id);
                             let recentSession = bookSessions[bookSessions.length - 1];
+                           // let percentComplete = (recentSession.page/recentSession.page_count);
 
                             return (
-                                <tr key={book.id}>
-                                    <td>{book.title}</td>
-                                    <td>{book.author}</td>
-                                    <td>{recentSession ? recentSession.page : <span>---</span>}</td>
-                                    <td>{recentSession ? recentSession.date.split('T')[0] : <span>---</span>}</td>
-                                    <td>CHANGE THIS</td>
-                                    <td><Button variant='outlined' onClick={() => history.push(`/edit/${book.id}`)}>EDIT</Button></td>
-                                    <td><Button variant='outlined' onClick={() => history.push(`/update/${book.id}`)}>UPDATE PROGRESS</Button></td>
-                                    <td><Button variant='outlined' onClick={() => history.push(`/complete/${book.id}`)}>COMPLETE</Button></td>
-                                </tr>
+                                <TableRow key={book.id}>
+                                    <TableCell>{book.title}</TableCell>
+                                    <TableCell>{book.author}</TableCell>
+                                    <TableCell>{recentSession ? recentSession.page : <span>---</span>}</TableCell>
+                                    <TableCell>{recentSession ? recentSession.date.split('T')[0] : <span>---</span>}</TableCell>
+                                    <TableCell>{recentSession ? (recentSession.page/recentSession.page_count*100).toFixed(2) : <span>---</span>}</TableCell>
+                                    <TableCell><Button variant='outlined' onClick={() => history.push(`/edit/${book.id}`)}>EDIT</Button></TableCell>
+                                    <TableCell><Button variant='outlined' onClick={() => history.push(`/update/${book.id}`)}>UPDATE PROGRESS</Button></TableCell>
+                                    <TableCell><Button variant='outlined' onClick={() => history.push(`/complete/${book.id}`)}>COMPLETE</Button></TableCell>
+                                </TableRow>
                             )
                         })}
-                    </tbody>
-                </table> : <p> You're not tracking any books yet! Click 'ADD BOOK' to get started!</p>}
+                    </TableBody>
+                </Table> : <p> You're not tracking any books yet! Click 'ADD BOOK' to get started!</p>}
                 <Button variant='contained' onClick={() => history.push('/addbook')}>ADD BOOK</Button>
             </Grid>
         </>
