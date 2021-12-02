f585eaf98a2e30882f3cd1e5fb0d8de645a65cf4
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-01 22:19:18 -0600

    removed old code

--- src/components/AddBook/AddBook.jsx
+++ src/components/AddBook/AddBook.jsx
@@ -35,7 +35,7 @@ function AddBook() {
                 justifyContent="center"
                 style={{ minHeight: '30vh' }}
             >
-                <h2 onClick={() => setNewBook({ title: '', author: '', page_count: '' })}>Add a Book</h2>
+                <h2>Add a Book</h2>
                 <form onSubmit={addNewBook}>
                     <FormControl
                         sx={{
