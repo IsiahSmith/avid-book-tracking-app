26ef18b28980b80ec721f5f4bd5a319371f0859b
Isiah Smith <isiahthomassmith@gmail.com>
2021-12-01 22:13:05 -0600

    cleaned up formatting

--- src/components/EditBook/EditBook.jsx
+++ src/components/EditBook/EditBook.jsx
@@ -11,7 +11,7 @@ import Grid from '@mui/material/Grid';
 function EditBook() {
     const dispatch = useDispatch();
     const history = useHistory();
-    const [updateBook, setUpdateBook] = useState({ title: 'The Catcher in the Rie', author: 'J. D. Salinger', page_count: '277' })
+    const [updateBook, setUpdateBook] = useState({ title: '', author: '', page_count: '' })
     const { book_id } = useParams();
 
     //Sets updateBook local state to the passed in inputs
