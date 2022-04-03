76b9fc5affc7db61d362a0737613460296435fd1
Isiah Smith <isiahthomassmith@gmail.com>
2022-04-02 20:29:37 -0500

    GET route made for goal

--- server/routes/goal.router.js
+++ server/routes/goal.router.js
@@ -3,3 +3,15 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
+// Gets goal from the logged in user
+router.get('/', rejectUnauthenticated, (req, res) => {
+    const queryText = `SELECT * FROM "goal" WHERE "user_id" = $1;`;
+    pool.query(queryText, [req.user.id])
+      .then(result => {
+        res.send(result.rows); //result.rows contains the users goal
+      })
+      .catch(err => {
+        console.log('Error getting goal', err);
+        res.sendStatus(500)
+      })
+  }); //End GET
